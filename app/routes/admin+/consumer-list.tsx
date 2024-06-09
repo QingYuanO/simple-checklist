d877f7a1c760e5261e7cd860458783270e89dc92
qingyuano <211720950@qq.com>
2024-06-09 17:42:24 +0800

    update

--- app/routes/admin+/consumer-list.tsx
+++ app/routes/admin+/consumer-list.tsx
@@ -1,10 +1,11 @@
-import { LoaderFunctionArgs, json } from '@remix-run/cloudflare';
+import { json, LoaderFunctionArgs } from '@remix-run/cloudflare';
 import { useLoaderData } from '@remix-run/react';
+import { authenticator } from '~/services/auth.server';
 import { Ghost } from 'lucide-react';
+
 import Header from '~/components/Header';
 import { Button } from '~/components/ui/button';
-import { Card, CardHeader, CardTitle, CardFooter } from '~/components/ui/card';
-import { authenticator } from '~/services/auth.server';
+import { Card, CardFooter, CardHeader, CardTitle } from '~/components/ui/card';
 
 export const loader = async ({ request, context }: LoaderFunctionArgs) => {
   const sessionUser = await authenticator.isAuthenticated(request, {
@@ -26,28 +27,20 @@ export default function ConsumerList() {
   const { users } = useLoaderData<typeof loader>();
 
   return (
-    <div className='py-14'>
-      <Header title='用户列表' isBack />
+    <div className="py-14">
+      <Header title="用户列表" isBack />
       {users && users.length > 0 ? (
-        <div className='flex flex-col gap-4 p-4'>
-          {users.map((user) => (
-            <Card key={user.id} className='col-span-1 flex flex-col justify-between'>
-              <CardHeader className='p-4 pb-2'>
+        <div className="flex flex-col gap-4 p-4">
+          {users.map(user => (
+            <Card key={user.id} className="col-span-1 flex flex-col justify-between">
+              <CardHeader className="p-4 pb-2">
                 <CardTitle>{user.name}</CardTitle>
                 {/* <CardDescription className='w-full'>{user.account}</CardDescription> */}
               </CardHeader>
-              <CardFooter className='flex justify-between px-4 pb-4'>
-                {user.phone ? (
-                  <Button size='sm' variant='secondary' asChild>
+              <CardFooter className="flex justify-end px-4 pb-4">
+                <div className="flex gap-3">
+                  <Button size="sm" variant="default" asChild>
                     <a href={`tel:${user.phone}`}>拨打电话</a>
-                  </Button>
-                ) : (
-                  <span></span>
-                )}
-
-                <div className='flex gap-3'>
-                  <Button size='sm' variant='default'>
-                    查看
                   </Button>
                 </div>
               </CardFooter>
@@ -55,10 +48,10 @@ export default function ConsumerList() {
           ))}
         </div>
       ) : (
-        <div className='mt-20 flex flex-col items-center gap-2'>
-          <Ghost className='size-8 text-zinc-800' />
-          <h3 className='font-semibold'>你还没有客户</h3>
-          <p className=''>快点邀请吧</p>
+        <div className="mt-20 flex flex-col items-center gap-2">
+          <Ghost className="size-8 text-zinc-800" />
+          <h3 className="font-semibold">你还没有客户</h3>
+          <p className="">快点邀请吧</p>
         </div>
       )}
     </div>
